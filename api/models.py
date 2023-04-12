33bd3469452fd2c4dbd09a72e9973ef015f8976d
Leonardo Barros <leonprogramer@gmail.com>
2023-04-12 11:19:30 -0300

    Initialization with serializers

--- api/models.py
+++ api/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class Son(models.Model):
+	nome = models.CharField(maxLength=120)
+	url = models.CharField(maxLength=150)
+	duracao = models.FloatField(maxLength=10)
+	tipo = models.CharField(maxLength=80)
+
+	def __str__(self):
+		return self.nome
+
+
+class PlayList(models.Model):
+	titulo = models.CharField(maxLength=80)
+	son = models.ManyToManyField()
+
+	def __str__(self):
+		return titulo
